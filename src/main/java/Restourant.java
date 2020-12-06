845d0ad3f1916da69f556c829ad04085cd604716
Vyacheslav Maratkanov <slava_maratkanov@mail.ru>
2020-12-06 14:41:01 +0500

    clear

--- src/main/java/Restourant.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class Restourant {
-
-   static Waiter waiter = new Waiter();
-   static Cooker cooker = new Cooker();
-   static Client client = new Client();
-}
